967266168d8dcf795ba57d4102816ede953d1946
David Schweikert <david@schweikert.ch>
2012-05-22 10:00:02 +0200

    remove generated files

---
+++ autoclean.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+rm -f Makefile
+rm -f Makefile.in
+rm -f aclocal.m4
+rm -rf autom4te.cache
+rm -f config.guess
+rm -f config.h
+rm -f config.h.in
+rm -f config.log
+rm -f config.status
+rm -f config.sub
+rm -f configure
+rm -f depcomp
+rm -f install-sh
+rm -f missing
+rm -f mkinstalldirs
+rm -f stamp-h1
+rm -f doc/Makefile.in
+rm -f src/Makefile.in

2f9cd95bbdb2944fe6139d851f6c763a846cec3f
David Schweikert <david@schweikert.ch>
2014-03-02 22:35:51 +0100

    test bintray deployment

--- ci/deploy-bintray.sh
+++ ci/deploy-bintray.sh
@@ -1,6 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 
 # upload to bintray.com/schweikert
 
-VERSION=3.9-rc1
+make dist
+VERSION=$(ls fping-*.tar.gz | sed -e 's/^fping-//' | sed -e 's/\.tar\.gz$//')
 curl -T fping-$VERSION.tar.gz -uschweikert:$BINTRAY_API_KEY https://api.bintray.com/content/schweikert/generic/fping-rc/$VERSION/fping-$VERSION.tar.gz

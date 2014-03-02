e9daacdf81de72382af767e5fd0957f0270aa92e
David Schweikert <david@schweikert.ch>
2014-03-02 22:50:50 +0100

    test bintray deployment

--- ci/deploy-bintray.sh
+++ ci/deploy-bintray.sh
@@ -4,4 +4,9 @@
 
 make dist
 VERSION=$(ls fping-*.tar.gz | sed -e 's/^fping-//' | sed -e 's/\.tar\.gz$//')
-curl -T fping-$VERSION.tar.gz -uschweikert:$BINTRAY_API_KEY https://api.bintray.com/content/schweikert/generic/fping-rc/$VERSION/fping-$VERSION.tar.gz
+if [[ "$VERSION" =~ [0-9]+\.[0-9]+ ]]; then
+    REPO=release
+else
+    REPO=beta
+fi
+curl -T fping-$VERSION.tar.gz -uschweikert:$BINTRAY_API_KEY https://api.bintray.com/content/schweikert/$RELEASE/fping/$VERSION/fping-$VERSION.tar.gz

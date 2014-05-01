9a5c6589b296a2ba00b4642564ab68baa8bf8374
David Schweikert <david@schweikert.ch>
2014-05-01 15:21:45 +0200

    re-order options in usage information

--- ci/test-2-help.pl
+++ ci/test-2-help.pl
@@ -22,6 +22,7 @@ Usage: fping [options] [targets...]
                 (ex. fping -g 192.168.1.0 192.168.1.255 or fping -g 192.168.1.0/24)
    -H n       Set the IP TTL value (Time To Live hops)
    -i n       interval between sending ping packets (in millisec) (default 25)
+   -I if      bind to a particular interface
    -l         loop sending pings forever
    -m         ping multiple interfaces on target host
    -n         show targets by name (-d is equivalent)
@@ -31,7 +32,6 @@ Usage: fping [options] [targets...]
    -Q n       same as -q, but show summary every n seconds
    -r n       number of retries (default 3)
    -s         print final stats
-   -I if      bind to a particular interface
    -S addr    set source address
    -t n       individual target initial timeout (in millisec) (default 500)
    -T n       ignored (for compatibility with fping 2.4)

c4c3068bbb62cb332d9ac093950a6f5a9218c74a
David Schweikert <david@schweikert.ch>
2014-05-04 21:22:28 +0200

    more tests

---
+++ ci/test-13-unknown-host.pl
@@ -0,0 +1,19 @@
+#!/usr/bin/perl -w
+
+use Test::Command tests => 6;
+
+# fping
+{
+my $cmd = Test::Command->new(cmd => "fping nosuchname.example.com");
+$cmd->exit_is_num(1);
+$cmd->stdout_is_eq("");
+$cmd->stderr_is_eq("nosuchname.example.com address not found\n");
+}
+
+# fping6
+{
+my $cmd = Test::Command->new(cmd => "fping6 nosuchname.example.com");
+$cmd->exit_is_num(1);
+$cmd->stdout_is_eq("");
+$cmd->stderr_is_eq("nosuchname.example.com: Name or service not known\n");
+}

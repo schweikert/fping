d91066ceebff327236e02d7c7867e1a1b1f299b9
David Schweikert <david@schweikert.ch>
2014-09-05 21:41:23 +1000

    trigger travis rebuild

--- ci/build-2-install.sh
+++ ci/build-2-install.sh
@@ -10,5 +10,9 @@ fi
 autoreconf -i
 ./configure --enable-ipv4 --enable-ipv6 --prefix=/opt/fping
 make CFLAGS="-g -fprofile-arcs -ftest-coverage"
+pwd
+df -k .
+which setcap
+uname -a
 sudo setcap cap_net_raw+ep src/fping
 sudo setcap cap_net_raw+ep src/fping6

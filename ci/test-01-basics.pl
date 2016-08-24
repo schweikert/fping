9e1b32ba11a33dff78a6444827dde9976ef4f62a
David Schweikert <david@schweikert.ch>
2016-08-24 11:45:10 +0200

    try to fix tests on travis-ci (no ipv6?)

--- ci/test-01-basics.pl
+++ ci/test-01-basics.pl
@@ -3,22 +3,31 @@
 use Test::Command tests => 9;
 
 # ping 127.0.0.1
-my $cmd1 = Test::Command->new(cmd => "fping 127.0.0.1");
-$cmd1->exit_is_num(0);
-$cmd1->stdout_is_eq("127.0.0.1 is alive\n");
-$cmd1->stderr_is_eq("");
+{
+    my $cmd = Test::Command->new(cmd => "fping 127.0.0.1");
+    $cmd->exit_is_num(0);
+    $cmd->stdout_is_eq("127.0.0.1 is alive\n");
+    $cmd->stderr_is_eq("");
+}
 
 # ping ::1
-my $cmd2 = Test::Command->new(cmd => "fping6 ::1");
-$cmd2->exit_is_num(0);
-$cmd2->stdout_is_eq("::1 is alive\n");
-$cmd2->stderr_is_eq("");
+if(system("/sbin/ifconfig | grep inet6") != 0) {
+    skip 'No IPv6 on this host', 3;
+}
+else {
+    my $cmd = Test::Command->new(cmd => "fping6 ::1");
+    $cmd->exit_is_num(0);
+    $cmd->stdout_is_eq("::1 is alive\n");
+    $cmd->stderr_is_eq("");
+}
 
 # ping 3 times 127.0.0.1
-my $cmd3 = Test::Command->new(cmd => "fping -p 100 -C3 127.0.0.1");
-$cmd3->exit_is_num(0);
-$cmd3->stdout_like(qr{127\.0\.0\.1 : \[0\], 84 bytes, 0\.\d+ ms \(0\.\d+ avg, 0% loss\)
-127\.0\.0\.1 : \[1\], 84 bytes, 0\.\d+ ms \(0.\d+ avg, 0% loss\)
-127\.0\.0\.1 : \[2\], 84 bytes, 0\.\d+ ms \(0.\d+ avg, 0% loss\)
-});
-$cmd3->stderr_like(qr{127\.0\.0\.1 : 0\.\d+ 0\.\d+ 0\.\d+\n});
+{
+    my $cmd = Test::Command->new(cmd => "fping -p 100 -C3 127.0.0.1");
+    $cmd->exit_is_num(0);
+    $cmd->stdout_like(qr{127\.0\.0\.1 : \[0\], 84 bytes, 0\.\d+ ms \(0\.\d+ avg, 0% loss\)
+    127\.0\.0\.1 : \[1\], 84 bytes, 0\.\d+ ms \(0.\d+ avg, 0% loss\)
+    127\.0\.0\.1 : \[2\], 84 bytes, 0\.\d+ ms \(0.\d+ avg, 0% loss\)
+    });
+    $cmd->stderr_like(qr{127\.0\.0\.1 : 0\.\d+ 0\.\d+ 0\.\d+\n});
+}

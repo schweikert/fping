de9c88d966f01402641d9bc1a78c7b1ac3e6a0bc
David Schweikert <david@schweikert.ch>
2016-11-01 13:19:31 +0100

    test fix

--- ci/test-12-option-type.pl
+++ ci/test-12-option-type.pl
@@ -2,7 +2,7 @@
 
 use Test::Command tests => 36;
 
-for my $arg (qw(i b B c C H i O p Q r t)) {
+for my $arg (qw(b B c C H i O p Q r t)) {
     my $cmd = Test::Command->new(cmd => "fping -$arg xxx");
     $cmd->exit_is_num(1);
     $cmd->stdout_is_eq("");

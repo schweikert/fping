68e3f6d69232612178850846a608d10615379d8c
David Schweikert <david@schweikert.ch>
2016-11-01 13:34:02 +0100

    fix again test

--- ci/test-12-option-type.pl
+++ ci/test-12-option-type.pl
@@ -1,8 +1,8 @@
 #!/usr/bin/perl -w
 
-use Test::Command tests => 36;
+use Test::Command tests => 30;
 
-for my $arg (qw(b B c C H i O p Q r t)) {
+for my $arg (qw(b B c C H O p Q r t)) {
     my $cmd = Test::Command->new(cmd => "fping -$arg xxx");
     $cmd->exit_is_num(1);
     $cmd->stdout_is_eq("");

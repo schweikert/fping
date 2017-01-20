b5046cfa786bbf6f35005ef2f297ebb8bef02fec
David Schweikert <david@schweikert.ch>
2017-01-20 22:59:55 +0100

    Fix -M expecting an argument, when it shouldn't

--- ci/test-07-options-i-m.pl
+++ ci/test-07-options-i-m.pl
@@ -1,10 +1,11 @@
 #!/usr/bin/perl -w
 
-use Test::Command tests => 4;
+use Test::Command tests => 7;
 
 #  -i n       interval between sending ping packets (in millisec) (default 25)
 #  -l         loop sending pings forever
 #  -m         ping multiple interfaces on target host
+#  -M         don't fragment
 
 # fping -i n
 {
@@ -20,6 +21,14 @@ my $cmd = Test::Command->new(cmd => '(sleep 2; pkill fping)& fping -p 900 -l 127
 $cmd->stdout_like(qr{127\.0\.0\.1 : \[0\], 84 bytes, 0\.\d+ ms \(0.\d+ avg, 0% loss\)
 127\.0\.0\.1 : \[1\], 84 bytes, 0\.\d+ ms \(0\.\d+ avg, 0% loss\)
 });
+}
+
+# fping -M
+{
+my $cmd = Test::Command->new(cmd => "fping -M 127.0.0.1");
+$cmd->exit_is_num(0);
+$cmd->stdout_is_eq("127.0.0.1 is alive\n");
+$cmd->stderr_is_eq("");
 }
 
 # fping -m -> test-14-internet-hosts

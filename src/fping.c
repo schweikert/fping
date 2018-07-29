b4277e33a9b609577e321494ac431df409db1d63
David Schweikert <david@schweikert.ch>
2018-07-29 08:44:47 +0200

    fix --help test, prepare 4.1 release

--- src/fping.c
+++ src/fping.c
@@ -354,6 +354,7 @@ int main(int argc, char** argv)
 
     /* pre-parse -h/--help, so that we also can output help information
      * without trying to open the socket, which might fail */
+    prog = argv[0];
     if(argc == 2 && ( strcmp(argv[1],"-h")==0 || strcmp(argv[1],"--help")==0 )) {
         usage(0);
     }
@@ -369,7 +370,6 @@ int main(int argc, char** argv)
             perror("cannot setuid");
     }
 
-    prog = argv[0];
     optparse_init(&optparse_state, argv);
     ident = getpid() & 0xFFFF;
     verbose_flag = 1;

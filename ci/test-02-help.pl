5d70c4d61b2e8c292337fefd975b9d603c6fdc8e
Erik Auerswald <auerswal@unix-ag.uni-kl.de>
2024-05-19 17:42:31 +0200

    test help output during normal option parsing
    The existing test for "fping -h" triggers the special code path
    for only one option, either -h or --help, that is handled before
    normal option parsing.

--- ci/test-02-help.pl
+++ ci/test-02-help.pl
@@ -1,11 +1,11 @@
 #!/usr/bin/perl -w
 
-use Test::Command tests => 9;
+use Test::Command tests => 12;
 
 my $I_HELP = "   -I, --iface=IFACE  bind to a particular interface\n";
 $I_HELP = '' if $^O eq 'darwin';
 
-# fping -h
+# fping -h (special pre-parse code path)
 my $cmd1 = Test::Command->new(cmd => "fping -h");
 $cmd1->exit_is_num(0);
 $cmd1->stdout_like(qr{Usage: fping \[options\] \[targets\.\.\.\]
@@ -15,6 +15,17 @@ Probing options:
    -v, --version      show version
 }s);
 $cmd1->stderr_is_eq("");
+
+# fping -4 -h (normal option parsing code path)
+my $cmd4 = Test::Command->new(cmd => "fping -4 -h");
+$cmd4->exit_is_num(0);
+$cmd4->stdout_like(qr{Usage: fping \[options\] \[targets\.\.\.\]
+
+Probing options:
+.*
+   -v, --version      show version
+}s);
+$cmd4->stderr_is_eq("");
 
 # fping -v
 my $cmd2 = Test::Command->new(cmd => "fping -v");

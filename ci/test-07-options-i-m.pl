41e422cb16d6697af9e99bf48f2b4bc876fe694d
Erik Auerswald <auerswal@unix-ag.uni-kl.de>
2024-06-23 17:13:36 +0200

    ci: relax timing requirements of two loop tests
    Sometimes, the macOS CI runner fails one of the loop tests.
    It seems as if the process is killed before it could finish
    the output required for the test to pass.  So give the test
    a bit more time between producing output and killing fping.

--- ci/test-07-options-i-m.pl
+++ ci/test-07-options-i-m.pl
@@ -52,7 +52,7 @@ $cmd->stderr_like(qr{\[\d+:\d+:\d+\]
 
 # fping -l -Q
 {
-my $cmd = Test::Command->new(cmd => '(sleep 2; pkill fping)& fping -p 850 -l -Q 1 127.0.0.1');
+my $cmd = Test::Command->new(cmd => '(sleep 2; pkill fping)& fping -p 770 -l -Q 1 127.0.0.1');
 $cmd->stdout_is_eq("");
 $cmd->stderr_like(qr{\[\d\d:\d\d:\d\d\]
 127\.0\.0\.1 : xmt/rcv/%loss = \d/\d/\d%, min/avg/max = \d\.\d+/\d\.\d+/\d\.\d+
@@ -63,7 +63,7 @@ $cmd->stderr_like(qr{\[\d\d:\d\d:\d\d\]
 
 # fping -l -t
 {
-my $cmd = Test::Command->new(cmd => '(sleep 2; pkill fping)& fping -p 900 -t 1500 -l 127.0.0.1');
+my $cmd = Test::Command->new(cmd => '(sleep 2; pkill fping)& fping -p 850 -t 1500 -l 127.0.0.1');
 $cmd->stdout_like(qr{127\.0\.0\.1 : \[0\], 64 bytes, \d\.\d+ ms \(\d\.\d+ avg, 0% loss\)
 127\.0\.0\.1 : \[1\], 64 bytes, \d\.\d+ ms \(\d\.\d+ avg, 0% loss\)
 });

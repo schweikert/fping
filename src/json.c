a6d1a1e0d3bf117c31db68f87d5cab163db6e9e9
Josh <josh.f@iptelco.com.au>
2025-02-12 14:50:00 +1000

    docs: Add attribution header to json.c
    Credit original JSON implementation to Sebastian Wiesinger (gsnw-sebast)
    and note enhancements by AthenaNetworks. Include GPL license header
    to match project licensing.

--- src/json.c
+++ src/json.c
@@ -1,3 +1,29 @@
+/*
+ * fping: fast-ping, file-ping, favorite-ping, funky-ping
+ *
+ * JSON output functionality
+ *
+ * Original implementation by Sebastian Wiesinger (gsnw-sebast)
+ * https://github.com/gsnw-sebast
+ *
+ * Enhanced with pretty-print support and test coverage by
+ * Josh F (AthenaNetworks)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
 #include "json.h"
 #include <stdio.h>
 #include <stdlib.h>

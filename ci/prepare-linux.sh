24866e92a7637b4a32ceef82bf2cc7366e1bd1e7
David Schweikert <david@schweikert.ch>
2025-12-27 08:42:16 +0100

    ci/prepare-linux.sh: do not run setcap unless necessary

--- ci/prepare-linux.sh
+++ ci/prepare-linux.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-sudo setcap cap_net_raw,cap_net_admin+ep src/fping
+if ! /usr/sbin/getcap src/fping | grep -q "cap_net_admin,cap_net_raw=ep"; then
+    sudo setcap cap_net_raw,cap_net_admin+ep src/fping
+fi
 
 if [ -d "$PWD/src" ]; then
     if [[ ":$PATH:" != *":$PWD/src:"* ]]; then

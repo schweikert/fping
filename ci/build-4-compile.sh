f689c314d0071f53c3496cd005f194b72dbf36af
German Service Network <support@gsnw.de>
2026-01-01 10:43:03 +0100

    ci: Removing travis-ci

--- ci/build-4-compile.sh
+++ ci/build-4-compile.sh
@@ -11,17 +11,6 @@ fi
 autoreconf -i
 ./configure --enable-ipv4 --enable-ipv6 --enable-safe-limits --prefix=/opt/fping
 make CFLAGS="-g -O0 -fprofile-arcs -ftest-coverage"
-## setcap currently doesn't work anymore on travis-ci
-#sudo setcap cap_net_raw+ep src/fping
-## setcap debugging:
-#pwd
-#df -k .
-#which setcap
-#uname -a
-#mount
-#
-#sudo apt-get install strace
-#sudo strace setcap cap_net_raw+ep src/fping
 
 # use setuid, since setcap is not available
 sudo chown root src/fping

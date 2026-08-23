48cb332b64f5a3fa3e500452bc28638020c0436b
German Service Network <support@gsnw.de>
2026-08-23 09:19:05 +0200

    Fix getprotobyname missing on Android preventing use (by Paliak)

--- src/socket4.c
+++ src/socket4.c
@@ -59,20 +59,15 @@ static int outgoing_src_addr_set_ipv4 = 0;
 
 int open_ping_socket_ipv4(int *socktype)
 {
-    struct protoent* proto;
     int s;
-
-    /* confirm that ICMP is available on this machine */
-    if ((proto = getprotobyname("icmp")) == NULL)
-        crash_and_burn("icmp: unknown protocol");
 
     /* create raw socket for ICMP calls (ping) */
     *socktype = SOCK_RAW;
-    s = socket(AF_INET, *socktype, proto->p_proto);
+    s = socket(AF_INET, *socktype, IPPROTO_ICMP);
     if (s < 0) {
         /* try non-privileged icmp (works on Mac OSX without privileges, for example) */
         *socktype = SOCK_DGRAM;
-        s = socket(AF_INET, *socktype, proto->p_proto);
+        s = socket(AF_INET, *socktype, IPPROTO_ICMP);
         if (s < 0) {
             return -1;
         }
